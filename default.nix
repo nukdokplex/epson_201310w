b96b0138eaaf4fe5e85ecc81a623c69fcb910287
nukdokplex <nukdokplex@nukdokplex.ru>
2024-07-17 22:26:51 +0500

    migrating from prebuilt to src rpm

--- default.nix
+++ default.nix
@@ -5,30 +5,35 @@ pkgs.stdenv.mkDerivation rec {
 
   src = pkgs.fetchurl {
     urls = [
-      "https://download3.ebz.epson.net/dsc/f/03/00/15/64/77/a25bcc458699e11aef84771caf6076628d56bf01/epson-inkjet-printer-201310w-${version}-1.x86_64.rpm"
-      "https://web.archive.org/web/https://download3.ebz.epson.net/dsc/f/03/00/15/64/77/a25bcc458699e11aef84771caf6076628d56bf01/epson-inkjet-printer-201310w-${version}-1.x86_64.rpm"
+      "https://download3.ebz.epson.net/dsc/f/03/00/15/64/77/6f825c323a9f4b8429e3f9f0f4e9c50b1b15f583/epson-inkjet-printer-201310w-${version}-1.src.rpm"
+      "https://web.archive.org/web/https://download3.ebz.epson.net/dsc/f/03/00/15/64/77/6f825c323a9f4b8429e3f9f0f4e9c50b1b15f583/epson-inkjet-printer-201310w-${version}-1.src.rpm"
     ];
-    hash = "sha256-uapglDUSwGzDyJ0lOAK2bHtY9jeGWjnHD1V28aaCVPk=";
+    hash = "sha256-596qMon5KmMHe7mh+ykdpWeazJOCGA6HmmtvdGheRmA=";
   };
-
-  dontBuild = true;
   
-  nativeBuildInputs = with pkgs; [ autoPatchelfHook ];
-  buildInputs = with pkgs; [ rpmextract gzip cups ];
+  nativeBuildInputs = with pkgs; [ rpmextract autoreconfHook file ];
+  buildInputs = with pkgs; [ libjpeg cups ];
 
   unpackPhase = ''
     rpmextract $src
-    cd opt/epson-inkjet-printer-201310w
-    gzip --decompress ppds/Epson/Epson-L120_Series-epson-driver.ppd.gz
-    substituteInPlace ppds/Epson/Epson-L120_Series-epson-driver.ppd --replace "/opt/epson-inkjet-printer-201310w/cups/lib/filter" "$out/lib/cups/filter"
-    substituteInPlace ppds/Epson/Epson-L120_Series-epson-driver.ppd --replace "/home/epson/projects/PrinterDriver/P2/_rpmbuild/SOURCES/epson-inkjet-printer-201310w-1.0.1/watermark" "$out/watermark"
+    tar -zxf epson-inkjet-printer-201310w-1.0.1.tar.gz
+    tar -zxf epson-inkjet-printer-filter-1.0.2.tar.gz
+    substituteInPlace epson-inkjet-printer-201310w-1.0.1/ppds/EPSON_L120.ppd --replace "/home/epson/projects/PrinterDriver/P2/_rpmbuild/SOURCES/epson-inkjet-printer-201310w-1.0.1/watermark" "$out/watermark"
+    cd epson-inkjet-printer-filter-1.0.2
+  '';
+
+  preConfigure = ''
+    chmod +x configure
+    export LDFLAGS="$LDFLAGS -Wl,--no-as-needed"
   '';
 
   postInstall = ''
+    cd ../epson-inkjet-printer-201310w-1.0.1
+    cp -a lib64 resource watermark $out
     mkdir -p $out/share/cups/model
-    cp -a ppds/Epson/Epson-L120_Series-epson-driver.ppd $out/share/cups/model
-    cp -a doc lib64 resource watermark $out
-    mkdir -p $out/lib/cups/filter
-    cp -a cups/lib/filter/epson_inkjet_printer_filter $out/lib/cups/filter
+    cp -a ppds/EPSON_L120.ppd $out/share/cups/model
+    mkdir -p $out/doc
+    cp -a Manual.txt $out/doc
+    cp -a README $out/doc/README.driver
   '';
 }
